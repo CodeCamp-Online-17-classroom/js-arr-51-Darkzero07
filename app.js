a9877d76a276946aa82230dd4efd80af6150fb1a
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 18:11:25 +0700

    js lab 137

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+// จงกระจาย element ที่ซ้อนกันอยู่ภายใน array ให้กระจายออกมาที่ element ชั้นนอกสุด
+
+let flattened = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+];
+
+const result = flattened.flat();
+console.log(result);
+// expected result: [0, 1, 2, 3, 4, 5]
